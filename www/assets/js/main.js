b7847bc6c748683cd2d3e698c2df73339bf09497
Jak <jaksawesome10@github.com>
2019-01-20 11:04:15 -0500

    stuff

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -23,6 +23,24 @@ function initiateStuff(){
 		
 		screen.orientation.lock('portrait');
 		
+		function successFunction(){
+			
+			console.info("It worked!");
+			
+		}
+		
+		function errorFunction(error){
+			
+			console.error(error);
+			
+		}
+		
+		function trace(value){
+			
+			console.log(value);
+			
+		}
+		
 		AndroidFullScreen.leanMode(successFunction, errorFunction);
 		
 	}
