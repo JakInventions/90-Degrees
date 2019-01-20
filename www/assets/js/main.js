d499531e563a9e89cc562bcd59f7fce88ab1e3ab
Jak <jaksawesome10@github.com>
2019-01-20 10:59:35 -0500

    full screen stuff

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,4 +1,32 @@
 
-
+function initiateStuff(){
+	
+	transitionIn('gameCanvas');
+	
+	if(!!window.cordova){
+		
+        console.log("In cordova");
+		
+		inCordova = true;
+		
+    }
+	
+   else{
+		
+		console.log("Not in cordova");
+		
+		inCordova = false;
+		
+	}
+	
+	if(inCordova){
+		
+		screen.orientation.lock('portrait');
+		
+		AndroidFullScreen.leanMode(successFunction, errorFunction);
+		
+	}
+	
+}
 
 
